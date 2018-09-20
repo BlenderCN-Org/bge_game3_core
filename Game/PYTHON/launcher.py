c68e32a5f129e78fd9c287d9d683d666b4b82003
DaedalusMDW <26656082+DaedalusMDW@users.noreply.github.com>
2018-09-20 13:13:24 -0500

    Update launcher.py

--- Game/PYTHON/launcher.py
+++ Game/PYTHON/launcher.py
@@ -594,7 +594,7 @@ def PLAYER(args=[], kwa=None):
 
 	"""Change Player Class"""
 
-	PL = ["Actor", "Raptor", "Sandstorm", "Wave"]
+	PL = ["Actor", "Red", "Blue", "Purple"]
 
 	if kwa != None:
 		if logic.CLASS.que[0] == "CLASS":
