1603398c74cdb81b72baeb75f6f72c5a2a1658d6
DaedalusMDW <26656082+DaedalusMDW@users.noreply.github.com>
2018-10-31 01:29:05 -0500

    use config

--- Game/PYTHON/stargate.py
+++ Game/PYTHON/stargate.py
@@ -9,21 +9,7 @@ import PYTHON.keymap as keymap
 if "STARGATE" not in base.LEVEL:
 	base.LEVEL["STARGATE"] = {}
 
-ADDRESS = {}
-
-ADDRESS["MILKYWAY"] = {
-"1A:1B:1C:1D:1E:1F:1P": "SGC Game",
-"2A:2B:2C:2D:2E:2F:1P": "Abydos Game",
-"1A:1B:1C:1D:1E:1F:1O:1P": "Midway",
-"2A:2B:2C:2D:2E:2F:1O:1P": "Wizards"
-}
-
-ADDRESS["PEGASUS"] = {
-"1A:1B:1C:1D:1E:1F:1P": "Atlantis Room",
-"2A:2B:2C:2D:2E:2F:1P": "Village Game",
-"1A:1B:1C:1D:1E:1F:1O:1P": "Midway",
-"2A:2B:2C:2D:2E:2F:1O:1P": "Wizards"
-}
+ADDRESS = base.settings.config.STARGATE_ADDRESS
 
 
 class IrisControl(base.CoreObject):
