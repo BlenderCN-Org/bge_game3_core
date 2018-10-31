02f795e33351dc50d4ebbb17c0347c175dd531a5
DaedalusMDW <26656082+DaedalusMDW@users.noreply.github.com>
2018-10-31 01:26:26 -0500

    Create config.py

---
+++ Game/PYTHON/config.py
@@ -0,0 +1,16 @@
+
+SCREENSHOT_PATH = "SCREENSHOTS"
+
+STARGATE_ADDRESS = {
+	"MILKYWAY":{
+		"1A:1B:1C:1D:1E:1F:1P": "LevelThree",
+		"2A:2B:2C:2D:2E:2F:1P": "LevelTwo",
+		"1A:1B:1C:1D:1E:1F:1O:1P": "Midway"
+	},
+
+	"PEGASUS":{
+		"1A:1B:1C:1D:1E:1F:1P": "Atlantis Room",
+		"2A:2B:2C:2D:2E:2F:1P": "Village Game",
+		"1A:1B:1C:1D:1E:1F:1O:1P": "Midway"
+	}
+}
